7966c12dc1e0418e88779d3ced2685f85cdc66c8
Hanz Luo <lhz0516@hotmail.com>
2016-08-29 18:30:43 -0700

    generate admin navigation automatically

--- client/components/admin/layout/nav.js
+++ client/components/admin/layout/nav.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default (props) => (
   <div>
     <a href="/admin">Admin</a> |
-    <a href="/admin/posts">Posts</a> |
+    {
+      _.values(props.context.configs).map((config, index) => (
+        <span key={index}><a href={config.adminUrl}>{config.displayName}</a> | </span>
+      ))
+    }
     <a href="/">Main Site</a> |
   </div>
 );
