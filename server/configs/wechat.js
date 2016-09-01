b5d53a0d2eec6049a2ed655c82426f0e1eec184f
Sean Song <songjinshan@gmail.com>
2016-09-01 13:49:31 +1200

    set up wechat test account in configs

--- server/configs/wechat.js
+++ server/configs/wechat.js
@@ -1,6 +1,6 @@
 export default {
   wechat: {
-    appSecret: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    appSecret: "061a568248ed8a1b3bcf51a91b94ae03",
     token: 'xxxxxxxxxxx',
     encodingAESKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   }
