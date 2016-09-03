8e2e9ee2678729413d0b2f213d7446a6c8710db0
Hanz Luo <lhz0516@hotmail.com>
2016-09-02 18:50:35 -0700

    home page ui

--- client/components/users/email-enrollment-form.js
+++ client/components/users/email-enrollment-form.js
@@ -16,10 +16,19 @@ export default class extends React.Component {
     if (!this.props.loggedIn) return <div>尚未登录，请登录后绑定邮箱</div>;
 
     return (
-      <form onSubmit={this.onSubmit}>
-        请填写登录用的邮箱地址，我们将发送一个链接到你邮箱，通过该链接设置登录密码
-        <input type="text" ref={(c) => this._input = c}/>
-        <input type="submit" value="提交"/>
-      </form>);
+      <div className="row">
+        <div className="col-sm-12">
+          <p>请填写登录用的邮箱地址，我们将发送一个链接到你邮箱，通过该链接设置登录密码</p>
+          <form onSubmit={this.onSubmit}>
+            <div className="input-group">
+              <input className="form-control" type="text" ref={(c) => this._input = c}/>
+              <span className="input-group-btn">
+                <button className="btn btn-default" type="submit">提交</button>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
