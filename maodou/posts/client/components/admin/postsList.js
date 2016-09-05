34612cad209f0d97cd1896d75e332a50f38c8e4e
Hanz Luo <lhz0516@hotmail.com>
2016-09-05 16:53:10 -0700

    add loading component to every page that need loading

--- maodou/posts/client/components/admin/postsList.js
+++ maodou/posts/client/components/admin/postsList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Loading from '/client/components/common/loading';
 
 export default (props) => {
   return (
@@ -10,31 +11,33 @@ export default (props) => {
       </div>
       <div className="row">
         {
-          props.posts.map((post, index) =>
-            <div key={index} className="col-sm-12">
-              <div className="hpanel blog-box">
-                <div className="panel-heading">
-                  <div className="media clearfix">
-                    <div className="media-body">
-                      <small className="text-muted">21.03.2015, 06:45 pm</small>
+          props.posts.length > 0 ?
+            props.posts.map((post, index) =>
+              <div key={index} className="col-sm-12">
+                <div className="hpanel blog-box">
+                  <div className="panel-heading">
+                    <div className="media clearfix">
+                      <div className="media-body">
+                        <small className="text-muted">21.03.2015, 06:45 pm</small>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="panel-body">
-                  <a href={`/posts/${post._id}`}>
-                    <h4>[{post.category}] {post.title}</h4>
-                    <p>{post.content}</p>
-                  </a>
-                </div>
-                <div className="panel-footer">
-                <span className="pull-right">
-                    <i className="fa fa-comments-o" /> 22 comments
-                </span>
-                  <i className="fa fa-eye" /> 142 views
+                  <div className="panel-body">
+                    <a href={`/posts/${post._id}`}>
+                      <h4>[{post.category}] {post.title}</h4>
+                      <p>{post.content}</p>
+                    </a>
+                  </div>
+                  <div className="panel-footer">
+                  <span className="pull-right">
+                      <i className="fa fa-comments-o" /> 22 comments
+                  </span>
+                    <i className="fa fa-eye" /> 142 views
+                  </div>
                 </div>
               </div>
-            </div>
-          )
+            ) :
+            <Loading />
         }
       </div>
     </div>
