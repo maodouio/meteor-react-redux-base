dbb39751b36ae59db6f7fee306e4f5b6a7ffadce
hanz <lhz0516@gmail.com>
2016-09-10 02:59:27 -0700

    add some core configs; add home page title

--- server/publications/index.js
+++ server/publications/index.js
@@ -1,3 +1,12 @@
-export default () => {
+export default ({ Meteor, Collections }) => {
+  const { Packages } = Collections;
 
+  Meteor.publish('core.configs.user', function () {
+    // TODO: Add configs filter for normal users
+    return Packages.find({ name: 'core' });
+  });
+
+  Meteor.publish('core.configs.admin', function () {
+    return Packages.find({ name: 'core' });
+  });
 }
