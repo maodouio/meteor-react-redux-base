43a251b3c321a9c0ee03b2182e9217c7519bc7bd
hanz <lhz0516@gmail.com>
2016-09-14 12:13:45 -0700

    debug needed: cannot import Qiniu

--- server/context.js
+++ server/context.js
@@ -1,9 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
+import Qiniu from 'qiniu';
 
 export default function() {
   return {
     Meteor,
-    Roles
+    Roles,
+    Qiniu
   };
 }
