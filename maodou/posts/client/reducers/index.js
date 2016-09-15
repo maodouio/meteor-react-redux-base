1e3ffed3ce56a4ac87351e04f5d93c69b015975a
Hanz Luo <lhz0516@hotmail.com>
2016-09-14 23:45:55 -0700

    upload post cover image; add plupload

--- maodou/posts/client/reducers/index.js
+++ maodou/posts/client/reducers/index.js
@@ -1,3 +1,10 @@
 export default {
-
+  postTmpCover(state = '', action) {
+    switch (action.type){
+      case 'ADD_POST_COVER':
+        return action.url;
+      default:
+        return state;
+    }
+  }
 };
--- server/methods/index.js
+++ server/methods/index.js
@@ -1,5 +1,7 @@
 import users from './users';
+import files from './files';
 
 export default (context) => {
   users(context);
+  files(context);
 }
