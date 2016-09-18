905e7b086d822d539deb49b5245622946dc599c5
Hanz Luo <lhz0516@hotmail.com>
2016-09-18 13:43:30 -0700

    add wechat  submodule

--- maodou/wechat/client/admin-routes.js
+++
@@ -1,5 +0,0 @@
-import WechatAdmin from './containers/admin';
-
-export default function (injectDeps, { configs }) {
-  return [{path: 'wechat', component: WechatAdmin}];
-}
--- maodou/wechat/client/containers/admin/index.js
+++
@@ -1,7 +0,0 @@
-import { useDeps } from 'react-simple-di';
-import { compose, withHandlers, withTracker, withRedux, composeAll } from 'react-komposer-plus';
-
-import WechatAdmin from '../../components/admin';
-
-
-export default composeAll()(WechatAdmin);
