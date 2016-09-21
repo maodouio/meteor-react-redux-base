7749b92e80ca6f6d36a4fe8586b7caa1771395ff
Sean Song <songjinshan@gmail.com>
2016-09-21 17:40:15 +1200

    fix reset password form being hidden

--- client/components/users/reset-password.js
+++ client/components/users/reset-password.js
@@ -25,7 +25,7 @@ export default class extends React.Component {
     if (this.state.uiState === 'SUCCESS') return <div>密码设置成功</div>;
 
     return (
-      <form onSubmit={this.onSubmit}>
+      <form style={{marginTop: 60, marginLeft: 10}} onSubmit={this.onSubmit}>
         {this.state.uiState === 'FAIL' && <p>密码设置失败，请重试</p>}
         密码：<input type="password" ref={(c) => this._input = c}/>
         <input type="submit" value="设置" />
