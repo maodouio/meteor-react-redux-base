458dad5b7720fb03b55494842e953a90f96b7422
wuyang910217 <wuyang910217@gmail.com>
2016-11-03 14:27:18 +0800

    add default qiniu key

--- server/configs/core.js
+++ server/configs/core.js
@@ -5,8 +5,8 @@ export default {
     qiniu: {
       ACCESS_KEY: Meteor.settings.core.qiniu.ACCESS_KEY,
       SECRET_KEY: Meteor.settings.core.qiniu.SECRET_KEY,
-      BUCKET_NAME: "maodou-base-test",
-      DOMAIN_NAME: "http://odhge55gv.bkt.clouddn.com/"
+      BUCKET_NAME: "maodou-base",
+      DOMAIN_NAME: "http://og0f8itra.bkt.clouddn.com/"
     }
   }
 };
