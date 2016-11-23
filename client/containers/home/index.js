f98630de5955740d390f2b19e5d88c5322b01e0e
wuyang910217 <wuyang910217@gmail.com>
2016-11-23 16:38:14 +0800

    seperate wechat login

--- client/containers/home/index.js
+++ client/containers/home/index.js
@@ -6,9 +6,18 @@ import Home from '../../components/home';
 
 function composer({context}, onData)  {
   const user = context().Meteor.user();
-
   if (user && Array.isArray(user.emails) && user.emails.length > 0) {
-    onData(null, {loggedIn: !!user, email: user.emails[0].address})
+    const isWechatVerified = user.profile.headimgurl && user.emails[0].verified;
+    const isWechat = user.profile.headimgurl || user.city;
+    const nickname = user.profile.nickname;
+    const avatar = isWechat ? user.profile.headimgurl : '';
+    onData(null, {
+      loggedIn: !!user,
+      notWechat: !isWechat,
+      isWechatVerified,
+      nickname,
+      avatar
+    });
   } else {
     onData(null, {loggedIn: user});
   }
