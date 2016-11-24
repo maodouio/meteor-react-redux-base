aea0677a90f68d8d260766ef8d70380852b55138
wuyang910217 <wuyang910217@gmail.com>
2016-11-24 12:05:46 +0800

    add toastr options

--- client/actions/users.js
+++ client/actions/users.js
@@ -4,28 +4,19 @@ export default {
   enrollWithEmail({Meteor}, email, callback) {
     Meteor.call("users.enrollWithEmail", email, callback);
   },
-  loginWithPassword({ Meteor, swal, toastr }, email, password, callback) {
+  loginWithPassword({ Meteor, toastr }, email, password, callback) {
     Meteor.loginWithPassword(email, password, (err) => {
       if (err) {
         console.log(err);
         if (err.reason === "User not found") {
-          swal({
-            title: '登录失败，用户不存在',
-            type: 'error'
-          });
+          toastr.error("登录失败，用户不存在");
         } else if (err.reason === "Incorrect password") {
-          swal({
-            title: '登录失败，密码错误',
-            type: 'error'
-          });
+          toastr.error("登录失败，密码错误");
         } else {
-          swal({
-            title: '登录失败',
-            type: 'error'
-          });
+          toastr.error("登录失败");
         }
       } else {
-        toastr["success"]("登录成功", "Success!");
+        toastr.success("登录成功");
       }
     });
   },
@@ -33,7 +24,7 @@ export default {
   resetPassword({Accounts}, token, password, callback) {
     Accounts.resetPassword(token, password, callback);
   },
-  createNewUser({ Meteor, Accounts, toastr, swal }, event) {
+  createNewUser({ Meteor, Accounts, toastr }, event) {
     return () => {
       event.preventDefault();
       const username = event.target.username.value;
@@ -41,58 +32,34 @@ export default {
       const password = event.target.password.value;
       const data = { username, email, password };
       if (username === '') {
-        swal({
-          title: '注册失败，用户名不能为空',
-          type: 'error'
-        });
+        toastr.info("用户名不能为空");
         return;
       }
       if (email === '') {
-        swal({
-          title: '注册失败，邮箱不能为空',
-          type: 'error'
-        });
+        toastr.info("邮箱不能为空");
         return;
       }
       if (password.length < 6) {
-        swal({
-          title: '注册失败，密码长度必须６位以上',
-          type: 'error'
-        });
+        toastr.info("密码长度必须６位以上");
         return;
       }
       Meteor.call('users.register', data, (err) => {
         if (!err) {
-          toastr["success"]("注册成功", "Success!");
+          toastr["success"]("注册成功");
           Meteor.loginWithPassword(email, password);
           browserHistory.push('/');
         } else {
           console.log(err.reason);
           if (err.reason === "Username already exists.") {
-            swal({
-              title: '注册失败，用户名已经存在',
-              type: 'error'
-            });
+            toastr.info("用户名已经存在");
           } else if (err.reason === "Email already exists.") {
-            swal({
-              title: '注册失败，邮箱已经被注册了',
-              type: 'error'
-            });
+            toastr.info("邮箱已经被注册了");
           } else if (err.reason === "Couldn't send verify email") {
-            swal({
-              title: '注册失败，不能发送验证邮箱',
-              type: 'error'
-            });
+            toastr.error("注册失败，不能发送验证邮箱");
           } else if (err.reason === "Need to set a username or email") {
-            swal({
-              title: '注册失败，请填写用户名或邮箱',
-              type: 'error'
-            });
+            toastr.info("请填写用户名或邮箱");
           } else {
-            swal({
-              title: '注册失败',
-              type: 'error'
-            });
+            toastr.info("注册失败");
           }
         }
       });
--- client/containers/admin/layout/index.js
+++ client/containers/admin/layout/index.js
@@ -4,14 +4,11 @@ import { browserHistory } from 'react-router';
 import Layout from '../../../components/admin/layout';
 
 const subscriptions = ({ context }, onData) => {
-  const { Meteor, swal } = context;
+  const { Meteor, toastr } = context;
   Meteor.call('validateAdmin', Meteor.user(), (err) => {
     if (err) {
-      browserHistory.push('/')
-      swal({
-        title: '当前用户无权访问',
-        type: 'error'
-      })
+      browserHistory.push('/');
+      toastr.error("当前用户无权访问");
     } else {
       _.keys(context.configs).map((module) => {
         Meteor.subscribe(`${module}.configs`)
