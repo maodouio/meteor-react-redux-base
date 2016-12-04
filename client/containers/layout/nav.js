c17565f70a1a6ec56efb16903cd8f7dc9053409b
wuyang910217 <wuyang910217@gmail.com>
2016-12-04 22:07:16 +0800

    merge new UI

--- client/containers/layout/nav.js
+++ client/containers/layout/nav.js
@@ -1,48 +1,16 @@
 import { useDeps } from 'react-simple-di';
-import { withHandlers, withTracker, withRedux, composeAll, withLifecycle } from 'react-komposer-plus';
+import { withTracker, withRedux, composeAll } from 'react-komposer-plus';
 import Nav from '../../components/layout/nav';
-
-const userEvents = {
-  setLang({ context, setLanguage }, event, lang) {
-    event.preventDefault();
-    context.T.setTexts(context.i18n[lang]);
-    context.dispatch(setLanguage(lang));
-  },
-  login: ({ context }, event) => {
-
-  }
-};
-
-const lifecycle = {
-  componentDidMount() {
-    $('.dropdown-toggle').dropdown();
-
-  },
-  componentWillMount() {
-    const { dispatch, setNavBar } = this.props;
-    let setNavbarState = () => {
-      let innerWidth = window.innerWidth;
-      dispatch(setNavBar(innerWidth));
-    };
-    setNavbarState();
-    window.onresize = setNavbarState;
-  }
-};
 
 const subscriptions = ({ context }, onData) => {
   const { Meteor, Collections, Roles } = context;
   if (Meteor.subscribe('packages.list').ready()) {
-    const packages = Collections.Packages.find({display: true}, {fields: {'name': 1}}).fetch();
     const corePkg = Collections.Packages.findOne({ name: 'core' });
     if (corePkg) {
-      const nickname = Meteor.user() ? Meteor.user().profile.nickname : '';
       onData(null, {
         appName: corePkg.configs.appName,
         loggedIn: !!context.Meteor.user(),
-        nickname,
-        packages,
         isReady: true,
-        isAdmin: Roles.userIsInRole(Meteor.user(), ['admin']),
       });
     }
   } else {
@@ -51,20 +19,14 @@ const subscriptions = ({ context }, onData) => {
 };
 
 const mapStateToProps = (state) => ({
-  innerWidth: state.navbarWidth
 });
 
 const depsToProps = (context, actions) => ({
   context,
-  dispatch: context.dispatch,
-  setLanguage: actions.core.setLanguage,
-  setNavBar: actions.core.setNavBar
 });
 
 export default composeAll(
-  withLifecycle(lifecycle),
   withTracker(subscriptions),
-  withHandlers(userEvents),
   withRedux(mapStateToProps),
   useDeps(depsToProps)
 )(Nav);
--- maodou/posts/client/components/tabs.js
+++ maodou/posts/client/components/tabs.js
@@ -1,26 +1,110 @@
 import React from 'react';
+import { TabBar } from 'antd-mobile/dist/antd-mobile';
 
 export default class extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'redTab',
+      hidden: false,
+    };
+  }
+  renderContent() {
+    this.setState({
+      hidden: !this.state.hidden,
+    });
+  }
   render() {
     return (
-      <div className={`posts-tab ${this.props.position} ${this.props.color}`}>
-        <ul className="flex-container">
-          {
-            this.props.configs ?
-              this.props.configs.categories.map((cate, i) =>
-                <li
-                  key={i}
-                  onClick={(e) => this.props.dispatch(this.props.changeCategory(e, cate))}
-                  className={`${cate===this.props.category ? 'active' : ''} flex-item`}
-                >
-                  {cate}
-                </li>
-              ) :
-              <div />
-          }
-        </ul>
-      </div>
+      <TabBar
+        unselectedTintColor="#949494"
+        tintColor="#33A3F4"
+        barTintColor="white"
+        hidden={this.state.hidden}
+      >
+        <TabBar.Item
+          title="新闻"
+          key="新闻"
+          icon={require('../icons/news.png')}
+          selectedIcon={require('../icons/newsOn.png')}
+          selected={this.state.selectedTab === 'blueTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'blueTab',
+            });
+          }}
+          data-seed="logId"
+        >
+          {this.renderContent}
+        </TabBar.Item>
+        <TabBar.Item
+          icon={require('../icons/活动.png')}
+          selectedIcon={require('../icons/活动 (1).png')}
+          title="活动"
+          key="活动"
+          selected={this.state.selectedTab === 'redTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'redTab',
+            });
+          }}
+          data-seed="logId1"
+        >
+          {this.renderContent}
+        </TabBar.Item>
+        <TabBar.Item
+          icon={require('../icons/项目.png')}
+          selectedIcon={require('../icons/项目 (1).png')}
+          title="项目"
+          key="项目"
+          selected={this.state.selectedTab === 'greenTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'greenTab',
+            });
+          }}
+        >
+          {this.renderContent}
+        </TabBar.Item>
+        <TabBar.Item
+          icon={require('../icons/客户.png')}
+          selectedIcon={require('../icons/客户 (1).png')}
+          title="客户"
+          key="客户"
+          selected={this.state.selectedTab === 'yellowTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'yellowTab',
+            });
+          }}
+        >
+          {this.renderContent}
+        </TabBar.Item>
+      </TabBar>
     );
   }
 }
+// export default class extends React.Component {
+//
+//   render() {
+//     return (
+//       <div className={`posts-tab ${this.props.position} ${this.props.color}`}>
+//         <ul className="flex-container">
+//           {
+//             this.props.configs ?
+//               this.props.configs.categories.map((cate, i) =>
+//                 <li
+//                   key={i}
+//                   onClick={(e) => this.props.dispatch(this.props.changeCategory(e, cate))}
+//                   className={`${cate===this.props.category ? 'active' : ''} flex-item`}
+//                 >
+//                   {cate}
+//                 </li>
+//               ) :
+//               <div />
+//           }
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
