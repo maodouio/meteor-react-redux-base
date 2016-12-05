79ae1908dcd7bf7493a2db923c05bde1a7ed550c
wuyang910217 <wuyang910217@gmail.com>
2016-12-05 20:03:17 +0800

    add siteName of InstancesInfo

--- server/methods/core.js
+++ server/methods/core.js
@@ -1,3 +1,5 @@
+import { updateSiteName } from 'lib/helpers/instancesHelper';
+
 export default ({ Meteor, Collections }) => {
   const { Packages } = Collections;
   Meteor.methods(({
@@ -7,6 +9,7 @@ export default ({ Meteor, Collections }) => {
           'configs.appName': configs.appName
         }
       });
+      updateSiteName(configs.appName);
     },
     'setModuleName'(name, display) {
       // const limit = Packages.find({display: true, name: {$ne: 'core'}}).fetch();
