835fee282a4a0625e9e65a7bf36b142c5b7864e1
Martin1994 <deletetmr@gmail.com>
2016-12-07 14:38:59 +0800

    fixed CSS

--- client/components/users/register.js
+++ client/components/users/register.js
@@ -28,23 +28,23 @@ export default class Register extends Component {
                 <form onSubmit={this.onSubmit} id="loginForm">
                   <div className="form-group">
                     <label className="control-label">用户名</label>
-                    <input type="text" ref='username' placeholder="你的用户名" title="请输入用户名" className="form-control" />
+                    <input style={{fontSize: 22, height: 42}} type="text" ref='username' placeholder="你的用户名" title="请输入用户名" className="form-control" />
                   </div>
                   <div className="form-group">
                     <label className="control-label">手机号</label>
-                    <input type="text" ref='phone' title="请输入手机号" className="form-control" />
-                    <button className='btn btn-default'>发送验证码</button>
+                    <input style={{fontSize: 22, height: 42}} type="text" ref='phone' title="请输入手机号" className="form-control" />
+                    <button style={{fontSize: 32}} className='btn btn-default'>发送验证码</button>
                   </div>
                   <div className="form-group">
                     <label className="control-label">验证码</label>
-                    <input type="text"  ref='verifyCode' title="请输入手机验证码" className="form-control" />
+                    <input style={{fontSize: 22, height: 42}} type="text"  ref='verifyCode' title="请输入手机验证码" className="form-control" />
                   </div>
                   <div className="form-group">
                     <label className="control-label">密码</label>
-                    <input type="password" ref='password' title="请输入密码" placeholder="******" className="form-control" />
+                    <input style={{fontSize: 22, height: 42}} type="password" ref='password' title="请输入密码" placeholder="******" className="form-control" />
                   </div>
-                  <button type="submit" className="btn btn-success btn-block">立即注册</button>
-                  <Link className="btn btn-default btn-block" to="/login">已有账号，马上登录</Link>
+                  <button style={{fontSize: 32}} type="submit" className="btn btn-success btn-block">立即注册</button>
+                  <Link style={{fontSize: 32}} className="btn btn-default btn-block" to="/login">已有账号，马上登录</Link>
                 </form>
               </div>
             </div>
